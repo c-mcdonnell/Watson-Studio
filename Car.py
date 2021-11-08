51721ef3616d8aa8b428c34a6fc09b67618a906d
c-mcdonnell <51486514+c-mcdonnell@users.noreply.github.com>
2021-11-08 10:40:08 -0800

    Add files via upload

---
+++ Car.py
@@ -0,0 +1,16 @@
+class Car:
+
+    def __init__(self, maxSpeed, minSpeed, weight, isTheCarOn,condition):
+        self.maxSpeed = 100
+        self.minSpeed = 0
+        self.weight = 4079
+        self.isTheCarOn = False
+        self.condition = 'A'
+    
+
+    def printVariables():
+        print(maxSpeed)
+        print(minSpeed)
+        print(weight)
+        print(isTheCarOn)
+        print(condition)
